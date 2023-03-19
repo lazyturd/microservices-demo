eca82d01baf486b53bdee9b469700f7faf50c24a
Ayo <ayorexyshittu@gmail.com>
2023-03-20 00:18:01 +0100

    tf

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -29,7 +29,7 @@ resource "helm_release" "cert_manager" {
   # }
   
   values = [
-    "${file("helm_values/values-cert-manager.yaml")}"
+    "${file("microservices-demo/terraform/helm_values.tf/values-cert-manager.yaml")}"
   ]
 }
 
