b46381887368503d91a43d30edd21c47f8e682e1
Ayo <ayorexyshittu@gmail.com>
2023-03-20 09:54:00 +0100

    uh

--- Jenkinsfile
+++ Jenkinsfile
@@ -28,17 +28,16 @@ pipeline {
                     script {
                         dir('deploy/kubernetes') {
                             sh "aws eks update-kubeconfig --name myapp-eks-cluster"
-                            // sh "kubectl create namespace sock-shop"
-                            // sh "kubectl apply -f complete-demo.yaml"
-        //                     // sh "kubectl apply -f frontend_ingress.yaml"
-        //                     // sh "wget --save-headers -O- sock-shop.ayomide.shop"
-        //                     // sh "kubectl create -f staging_issuer.yaml"
-        //                     // sh "kubectl create -f prod_issuer.yaml"
-                            // sh "kubectl create -f manifests-monitoring"
-                            // sh "helm repo add prometheus-community https://prometheus-community.github.io/helm-charts"
-                            // sh "helm install mongodb-exporter prometheus-community/prometheus-mongodb-exporter -f values.yaml"
-                            // sh "kubectl create namespace nginx"
-                            sh "kubectl delete all --all -n nginx"
+                            sh "kubectl create namespace sock-shop"
+                            sh "kubectl apply -f complete-demo.yaml"
+                            sh "kubectl apply -f frontend_ingress.yaml"
+                            sh "wget --save-headers -O- sock-shop.ayomide.shop"
+                            sh "kubectl create -f staging_issuer.yaml"
+                            sh "kubectl create -f prod_issuer.yaml"
+                            sh "kubectl create -f manifests-monitoring"
+                            sh "helm repo add prometheus-community https://prometheus-community.github.io/helm-charts"
+                            sh "helm install mongodb-exporter prometheus-community/prometheus-mongodb-exporter -f values.yaml"
+                            sh "kubectl create namespace nginx"
                             sh  "kubectl apply -f my-nginx-deployment.yaml"
                         }
                     }
--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,77 +1,77 @@
-# resource "helm_release" "cert_manager" {
-#   name       = "cert-manager"
-#   repository = "https://charts.jetstack.io"
-#   chart      = "cert-manager"
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
 
-#   create_namespace = true
-#   namespace = "cert-manager"
+  create_namespace = true
+  namespace = "cert-manager"
 
 
-#   # set {
-#   #   name  = "wait-for"
-#   #   value = module.cert_manager_irsa_role.iam_roles_arn
+  # set {
+  #   name  = "wait-for"
+  #   value = module.cert_manager_irsa_role.iam_roles_arn
 
-#   # }
+  # }
 
-#   set {
-#     name  = "installCRDs"
-#     value = "true"
-#   }
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
 
-#   set {
-#     name  = "version"
-#     value = "v1.4.0"
-#   }
+  set {
+    name  = "version"
+    value = "v1.4.0"
+  }
 
-#   # set {
-#   #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com\\/role-arn"
-#   #   value = module.cert_manager_irsa_role.iam_roles_arn
-#   # }
+  # set {
+  #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com\\/role-arn"
+  #   value = module.cert_manager_irsa_role.iam_roles_arn
+  # }
   
-#   values = [
-#     "${file("helm_values/values-cert-manager.yaml")}"
-#   ]
-# }
+  values = [
+    "${file("helm_values/values-cert-manager.yaml")}"
+  ]
+}
 
-# resource "helm_release" "external_dns" {
-#   name       = "external-dns"
-#   repository = "https://charts.bitnami.com/bitnami"
-#   chart      = "external-dns"
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "external-dns"
 
-#   create_namespace = true
-#   namespace = "cert-manager"
+  create_namespace = true
+  namespace = "cert-manager"
 
 
-#   # set {
-#   #   name  = "wait-for"
-#   #   value = module.external_dns_irsa_role.iam_roles_arn
+  # set {
+  #   name  = "wait-for"
+  #   value = module.external_dns_irsa_role.iam_roles_arn
 
-#   # }
+  # }
 
-#   set {
-#     name  = "domainFilters"
-#     value = "{${local.domain}}"
-#   }
+  set {
+    name  = "domainFilters"
+    value = "{${local.domain}}"
+  }
 
-#   set {
-#     name  = "installCRDs"
-#     value = "true"
-#   }
-#   # set {
-#   #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com\\/role-arn"
-#   #   value = module.external_dns_irsa_role.iam_roles_arn
-#   # }
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+  # set {
+  #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com\\/role-arn"
+  #   value = module.external_dns_irsa_role.iam_roles_arn
+  # }
 
-#   # values = [
-#   #   "${file("helm_values/values-external-dns.yaml")}"
-#   # ]
-# }
+  # values = [
+  #   "${file("helm_values/values-external-dns.yaml")}"
+  # ]
+}
 
-# resource "helm_release" "nginx" {
-#   name  = "ingress-nginx"
-#   repository = "https://kubernetes.github.io/ingress-nginx"
-#   chart = "ingress-nginx"
+resource "helm_release" "nginx" {
+  name  = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
 
-#   create_namespace = true
-#   namespace = "ingress-nginx"
-# }
+  create_namespace = true
+  namespace = "ingress-nginx"
+}
